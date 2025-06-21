f5e94c4d280489588821d8aa33e66a5d882edf0b
Nabin <sharmanabin.0910@gmail.com>
2025-06-21 22:02:51 +0530

    Setting up all basic routes

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -5,4 +5,19 @@ router.route('/').get((req , res)=>{
     res.status(200).json({message : "Contacts are found "})
 })
 
+router.route('/').post((req , res)=>{
+    res.status(200).json({message : "Create Contact "})
+})
+router.route('/:id').get((req , res)=>{
+    res.status(200).json({message : `Get contact for ${req.params.id}`})
+})
+
+router.route('/:id').put((req , res)=>{
+    res.status(200).json({message : `Update contact for ${req.params.id}`})
+})
+
+router.route('/:id').delete((req , res)=>{
+    res.status(200).json({message : `Delete contact for ID ${req.params.id}`})
+})
+
 module.exports = router
